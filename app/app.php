64a0646e98b13a1f215c0b7341a8fee094997eed
Patrick McGreevy and Zach Swanson <zach@>
2017-02-07 10:33:22 -0800

    Print list of tasks

--- app/app.php
+++ app/app.php
@@ -7,7 +7,18 @@
     $app = new Silex\Application();
 
     $app->get('/', function() {
-        return 'Sample root page';
+        $test_task = new Task("Learn PHP.");
+        $another_task = new Task("Learn Drupal.");
+        $third_task = new Task("Visit France.");
+
+        $list_of_tasks = array($test_task, $another_task, $third_task);
+        $output = "";
+
+        foreach ($list_of_tasks as $task) {
+            $output = $output . "<p>" . $task->getDescription() . "<p>";
+        }
+
+        return $output;
     });
 
     return $app;
